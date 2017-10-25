d25111400ddb6f13945c1d3fc5a56764c3edea61
Laurent Bourgault-Roy <laurent.bourgault-roy@iaah.ca>
2017-10-25 11:18:50 -0400

    Even better recursive solution

--- src/main/java/ca/lbroy/kata/Bowling.java
+++ src/main/java/ca/lbroy/kata/Bowling.java
@@ -16,13 +16,13 @@ public class Bowling {
         if (rolls.size() <= 3) return rolls.stream().mapToInt(Integer::intValue).sum();
 
         if (rolls.get(0) == ALL_PINS) {
-            return rolls.get(0) + rolls.get(1) + rolls.get(2) + score(rolls.subList(1, rolls.size()));
+            return score(rolls.subList(0, 3)) + score(rolls.subList(1, rolls.size()));
         }
 
         if (rolls.get(0) + rolls.get(1) == ALL_PINS) {
-            return rolls.get(0) + rolls.get(1) + rolls.get(2) + score(rolls.subList(2, rolls.size()));
+            return score(rolls.subList(0, 3)) + score(rolls.subList(2, rolls.size()));
         }
 
-        return rolls.get(0) + rolls.get(1) + score(rolls.subList(2, rolls.size()));
+        return score(rolls.subList(0, 2)) + score(rolls.subList(2, rolls.size()));
     }
 }
