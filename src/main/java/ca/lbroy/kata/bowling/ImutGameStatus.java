cc25b1518cd1b7182def37ef0d6d6e576cfbf12b
Laurent Bourgault-Roy <laurent.bourgault-roy@iaah.ca>
2017-11-14 16:18:10 -0500

    ImutGameStatus now passes all its test

--- src/main/java/ca/lbroy/kata/bowling/ImutGameStatus.java
+++ src/main/java/ca/lbroy/kata/bowling/ImutGameStatus.java
@@ -1,5 +1,7 @@
 package ca.lbroy.kata.bowling;
 
+import static java.lang.Math.max;
+
 /**
  * Copyright 2017 IAAH - All right reserved
  *
@@ -7,70 +9,108 @@ package ca.lbroy.kata.bowling;
  */
 public class ImutGameStatus {
 
-    private final boolean spareBonus;
+    private final Bonus bonus;
     private final int score;
-    private final FrameStatus frameStatus;
+    private final Frame frame;
 
-    private ImutGameStatus(FrameStatus frameStatus, int score, boolean spareBonus) {
+    private ImutGameStatus(Frame frame, Bonus bonus, int score) {
         this.score = score;
-        this.frameStatus = frameStatus;
-        this.spareBonus = spareBonus;
+        this.frame = frame;
+        this.bonus = bonus;
     }
 
-    public static ImutGameStatus newGame() {
-        return new ImutGameStatus(FrameStatus.firstFrame(), 0, false);
+    public static ImutGameStatus start() {
+        return new ImutGameStatus(Frame.start(), Bonus.start(), 0);
     }
 
     public ImutGameStatus addRoll(int pins) {
         return new ImutGameStatus(
-                frameStatus.addRoll(pins),
-                nextScore(pins),
-                frameStatus.isSpare(pins)
+                frame.addRoll(pins),
+                frame.updateBonus(bonus, pins),
+                score + pins * scoreMultiplier()
         );
     }
 
-    private int nextScore(int pins) {
-        if (frameStatus.isBonusRoll() && spareBonus) {
-            return score + pins * 2;
-        }
-
-        return score + pins;
+    private int scoreMultiplier() {
+        if (frame.isExtraRoll()) return bonus.multiplier();
+        return 1 + bonus.multiplier();
     }
 
     public int score() {
         return score;
     }
 
-    private static class FrameStatus {
+    private static class Frame {
         private final int index;
         private final int score;
         private final int rolls;
 
-        public FrameStatus(int index, int score, int rolls) {
+        public Frame(int index, int score, int rolls) {
             this.index = index;
             this.score = score;
             this.rolls = rolls;
         }
 
-        private static FrameStatus firstFrame() {
-            return new FrameStatus(0, 0, 0);
+        private static Frame start() {
+            return new Frame(0, 0, 0);
         }
 
-        private FrameStatus addRoll(int pins) {
-            if (rolls == 1) {
-                return new FrameStatus(index + 1, 0, 0);
+        private Frame addRoll(int pins) {
+            if (isStrike(pins) || rolls == 1) {
+                return new Frame(index + 1, 0, 0);
             }
 
-            return new FrameStatus(index, pins, rolls + 1);
+            return new Frame(index, pins, rolls + 1);
         }
 
-        private boolean isBonusRoll() {
-            return index < 10;
+        private Bonus updateBonus(Bonus bonus, int rollPins) {
+            if (isExtraRoll()) return bonus.standardRoll();
+            if (isStrike(rollPins)) return bonus.strike();
+            if (isSpare(rollPins)) return bonus.spare();
+
+            return bonus.standardRoll();
+        }
+
+        private boolean isExtraRoll() {
+            return index >= 10;
         }
 
         private boolean isSpare(int rollPins) {
             return rolls == 1 && score + rollPins == 10;
+        }
 
+        private boolean isStrike(int rollPins) {
+            return rolls == 0 && rollPins == 10;
+        }
+    }
+
+    static class Bonus {
+        private final int currentRoll;
+        private final int nextRoll;
+
+        private Bonus(int currentRoll, int nextRoll) {
+            this.currentRoll = currentRoll;
+            this.nextRoll = nextRoll;
+        }
+
+        private static Bonus start() {
+            return new Bonus(0, 0);
+        }
+
+        private Bonus standardRoll() {
+            return new Bonus(nextRoll, 0);
+        }
+
+        private Bonus spare() {
+            return new Bonus(nextRoll + 1, 0);
+        }
+
+        private Bonus strike() {
+            return new Bonus(nextRoll + 1, 1);
+        }
+
+        private int multiplier() {
+            return currentRoll;
         }
     }
 }
--- src/test/java/ca/lbroy/kata/bowling/ImutBowlingTest.java
+++ src/test/java/ca/lbroy/kata/bowling/ImutBowlingTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertEquals;
 public class ImutBowlingTest {
     @Test
     public void allGutterGame() {
-        ImutGameStatus allGutterGame = addManyRoll(ImutGameStatus.newGame(), 20, 0);
+        ImutGameStatus allGutterGame = addManyRoll(ImutGameStatus.start(), 20, 0);
 
         assertEquals("All gutter game", 0, allGutterGame.score());
     }
@@ -26,7 +26,7 @@ public class ImutBowlingTest {
 
     @Test
     public void onePinPerRollGame() {
-        ImutGameStatus gameStatus = ImutGameStatus.newGame();
+        ImutGameStatus gameStatus = ImutGameStatus.start();
         gameStatus = addManyRoll(gameStatus, 20, 1);
 
         assertEquals("All gutter game", 20, gameStatus.score());
@@ -34,7 +34,7 @@ public class ImutBowlingTest {
 
     @Test
     public void oneSpareGame() {
-        ImutGameStatus oneSpare = addManyRoll(ImutGameStatus.newGame(), 3, 5);
+        ImutGameStatus oneSpare = addManyRoll(ImutGameStatus.start(), 3, 5);
         ImutGameStatus completedGame = addManyRoll(oneSpare, 17, 0);
 
         assertEquals("One spare game", 20, completedGame.score());
@@ -42,8 +42,26 @@ public class ImutBowlingTest {
 
     @Test
     public void allSpareGame() {
-        ImutGameStatus allSpareGame = addManyRoll(ImutGameStatus.newGame(), 21, 5);
+        ImutGameStatus allSpareGame = addManyRoll(ImutGameStatus.start(), 21, 5);
 
         assertEquals("One spare game", 150, allSpareGame.score());
+    }
+
+    @Test
+    public void oneStrikeGame() {
+        ImutGameStatus oneStrike = ImutGameStatus.start()
+                .addRoll(10)
+                .addRoll(4)
+                .addRoll(4);
+        ImutGameStatus oneStrikeCompleted = addManyRoll(oneStrike, 16, 0);
+
+        assertEquals("One strike game", 26, oneStrikeCompleted.score());
+    }
+
+    @Test
+    public void allStrikeGame() {
+        ImutGameStatus allStrikeGame = addManyRoll(ImutGameStatus.start(), 12, 10);
+
+        assertEquals("One strike game", 300, allStrikeGame.score());
     }
 }
